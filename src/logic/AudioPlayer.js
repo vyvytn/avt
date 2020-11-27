63219b0773140c77a95d27cf7b9709141646c579
Jonathan Neidel <jneidel@jneidel.com>
2020-11-27 10:59:00 +0100

    Add Equalizer class, update AudioPlayer
    Required for 24. in #14

--- src/logic/AudioPlayer.js
+++ src/logic/AudioPlayer.js
@@ -1,11 +1,17 @@
 export default class AudioPlayer {
+  // constructor( AudioContext, output ) - ctx shared among players
+  //   output isn't ctx.destination itself, but the master gainNode
+
   // current: Song
-  
   // play()
   // pause()
   // stop()
   // seek( secToSeekTo )
-  
+
+  // playlist: Playlist
+  // skip() - delegate to playlist
+  // prev() - delegate to playlist
+
   // setVolume( val ) - resets at song change
   // setTempo( val )  - resets at song change
 }
