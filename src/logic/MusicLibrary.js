844a479cf32d7eb01380598b9651301350cc53ab
Jonathan Neidel <jneidel@jneidel.com>
2020-11-27 10:33:12 +0100

    Adapt MusicLibrary for colors
    Required for 7. in #9

--- src/logic/MusicLibrary.js
+++ src/logic/MusicLibrary.js
@@ -1,4 +1,9 @@
 export default class MusicLibrary {
   // list: Song[]
   // insert( Song )
+
+  // playlists: Playlist[] - to maintain colors - 0=red, 1=green
+  // registerAsPlaylist( Playlist ): playlistId
+  // updatePlaylist( playlistId ) - tell the library that own playlist got updated (insert, delete)
+  //  so the library can update colors
 }
