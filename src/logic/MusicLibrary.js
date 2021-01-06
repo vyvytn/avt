f97f0b11e47024f89e2dc31f808416fc6b9217d5
Jonathan Neidel <jneidel@jneidel.com>
2021-01-06 10:52:19 +0100

    Add inserted index as return value to MusicLib.insert

--- src/logic/MusicLibrary.js
+++ src/logic/MusicLibrary.js
@@ -3,7 +3,7 @@ import Song from "./Song";
 export default class MusicLibrary {
   list = []; // Song[]
   insert( song ) {
-    this.list.push( song );
+    return this.list.push( song ) -1; // return index
   }
 
   playlists = [];
