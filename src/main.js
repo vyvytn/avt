1622f4153e503bd3f5b808d8f5f96221fa38b795
Jonathan Neidel <jneidel@jneidel.com>
2021-01-06 17:21:49 +0100

    Comment out Player example code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-import * as audioLogic from "./logic";
+// import * as audioLogic from "./logic"; // uncomment to run player example
 
 Vue.config.productionTip = false;
 
