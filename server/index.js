6954b2ce924a0b476621ba4e415adb1399e1c9a9
Jonathan Neidel <jneidel@jneidel.com>
2021-01-22 11:05:29 +0100

    Set cors header to whitelist everything

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ server.use( bodyParser.json() );
 server.use( bodyParser.urlencoded( { extended: true } ) );
 
 server.use( cors() ); // allow for access from vue router
+server.use( (req, res, next) => {
+  res.header( "Access-Control-Allow-Origin", "*" );
+  res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+  next();
+} );
+
 server.use( "/static", express.static( `${__dirname}/../static` ) );
 
 server.use( "/", require( "./routes" ) );
